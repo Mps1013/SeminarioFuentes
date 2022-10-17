456c922dcf5e3688a632211bae2141bbd6bf6c75
Mps1013 <mps1013@alu.ubu.es>
2022-10-17 11:31:40 +0200

    Todo bien

--- SeminarioFuentes.R
+++ SeminarioFuentes.R
@@ -7,16 +7,6 @@ ina <- read_delim("INPUT/DATA/47444.csv",
 sui <- read_delim("INPUT/DATA/02001bsc.csv",
                   delim = ";", escape_double = FALSE, trim_ws = TRUE,locale=locale(encoding="latin1"))
 
-
-
 print(desi)
 print(ina)
 print(sui)
-
-desi <- read_delim("INPUT/DATA/Desigualdad (S80_S20) (CCAA).csv", 
-                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
-sui <- read_delim("INPUT/DATA/02001bsc.csv", 
-                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
-ina <- read_delim("INPUT/DATA/47444.csv", 
-                  delim = ";", escape_double = FALSE, trim_ws = TRUE,locale=locale(encoding="latin1"))
-
