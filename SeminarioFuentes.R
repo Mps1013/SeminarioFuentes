a01e5d5c00324e73dcbc6d1eb7ac0e0ad3ea3249
FatimaEzzahraM <fmo1002@alu.ubu.es>
2022-10-17 11:12:42 +0200

    Arreglado el encoding

--- SeminarioFuentes.R
+++ SeminarioFuentes.R
@@ -4,4 +4,9 @@ desi <- read_delim("INPUT/DATA/Desigualdad (S80_S20) (CCAA).csv",
 ina <- read_delim("INPUT/DATA/47444.csv",
                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
 sui <- read_delim("INPUT/DATA/02001bsc.csv",
-                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
+                  delim = ";", escape_double = FALSE, trim_ws = TRUE,locale=locale(encoding="latin1"))
+
+
+print(desi)
+print(ina)
+print(sui)
