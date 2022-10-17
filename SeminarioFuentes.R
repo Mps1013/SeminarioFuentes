89393f20f65be0b74eaca37cf36587c47406cd89
Mps1013 <mps1013@alu.ubu.es>
2022-10-17 10:43:28 +0200

    .

--- SeminarioFuentes.R
+++ SeminarioFuentes.R
@@ -5,3 +5,5 @@ ina <- read_delim("INPUT/DATA/47444.csv",
                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
 sui <- read_delim("INPUT/DATA/02001bsc.csv", 
                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
+ina <- read_delim("SeminarioFuentes/INPUT/DATA/47444.csv", 
+                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
