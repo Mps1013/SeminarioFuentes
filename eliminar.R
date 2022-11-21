4e376a777a6afbbacdca20ee0a251bb9b976ef1f
Mps1013 <mps1013@alu.ubu.es>
2022-11-21 10:21:22 +0100

    Guardado de gráficos.

--- eliminar.R
+++ eliminar.R
@@ -8,3 +8,25 @@ ggsave(
   units = "cm",
   dpi = 320
 )
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad.jpg",
+  plot = suina_plot2,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
+
+ggsave(
+  filename = "Relación_Desigualdad_Innacesibilidad.jpg",
+  plot = desina_plot2,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
