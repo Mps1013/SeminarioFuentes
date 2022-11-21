914866b6ccb9926718592cdbbb984d2a5c93cd3c
Mps1013 <mps1013@alu.ubu.es>
2022-11-21 10:58:35 +0100

    Guardado de gráficas.

--- eliminar.R
+++ eliminar.R
@@ -10,7 +10,7 @@ ggsave(
 )
 
 ggsave(
-  filename = "Relación_Suicidios_Innacesibilidad.jpg",
+  filename = "Relación_Suicidios_Innacesibilidad_SaludMental.jpg",
   plot = suina_plot2,
   path = "OUTPUT/DATA", # ruta relativa
   scale = 0.5,
@@ -23,6 +23,39 @@ ggsave(
 ggsave(
   filename = "Relación_Desigualdad_Innacesibilidad.jpg",
   plot = desina_plot2,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad_AtencionMedica.jpg",
+  plot = suina2_plot2,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad_MedicamentoRecetado.jpg",
+  plot = suina3_plot2,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad_SaludMental_RL.jpg",
+  plot = suina_plot2.1,
   path = "OUTPUT/DATA", # ruta relativa
   scale = 0.5,
   width = 40,
