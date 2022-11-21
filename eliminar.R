df4fd13ef0601c47641960a8b8ab967ad95a7667
Mps1013 <mps1013@alu.ubu.es>
2022-11-21 11:01:53 +0100

    Guardado gráficas.

--- eliminar.R
+++ eliminar.R
@@ -63,3 +63,25 @@ ggsave(
   units = "cm",
   dpi = 320
 )
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad_AtencionMedica_RL.jpg",
+  plot = suina2_plot2.1,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
+
+ggsave(
+  filename = "Relación_Suicidios_Innacesibilidad_MedicamentoRecetado_RL.jpg",
+  plot = suina3_plot2.1,
+  path = "OUTPUT/DATA", # ruta relativa
+  scale = 0.5,
+  width = 40,
+  height = 30,
+  units = "cm",
+  dpi = 320
+)
